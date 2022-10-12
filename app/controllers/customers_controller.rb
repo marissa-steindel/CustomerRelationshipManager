d658a6a49a60fbafd70bd7c83a026625a0aa00f2
Marissa Steindel <marissa.steindel@gmail.com>
2022-10-12 17:06:49 -0500

    add pageName variable to customers controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,13 +1,16 @@
 class CustomersController < ApplicationController
   def index
+    @pageName = "Customers"
     @customers = Customer.all
   end
 
   def alphabetized
+    @pageName = "Customers - alphabetical order"
     @customers = Customer.order(:name)
   end
 
   def missing_email
+    @pageName = "Customers missing email address"
     @customers = Customer.where(email: '')
   end
 
