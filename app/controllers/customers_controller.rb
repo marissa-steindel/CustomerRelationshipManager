3b50799945bf0c6f62dc641d496c4d1de050f17e
Marissa Steindel <marissa.steindel@gmail.com>
2022-10-12 15:39:14 -0500

    create customer controller actions (functions)

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,14 @@
 class CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def alphabetized
+    @customers = Customer.order(:name)
+  end
+
+  def missing_email
+    @customers = Customer.where(email: '')
+  end
+
 end
