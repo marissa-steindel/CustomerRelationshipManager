e011d19225ed0d5a278c9397e0e3e6269766da90
Marissa Steindel <marissa.steindel@gmail.com>
2022-10-13 00:34:41 -0500

    set all controller actions to render :index view

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,11 +7,13 @@ class CustomersController < ApplicationController
   def alphabetized
     @pageName = "Customers - alphabetical order"
     @customers = Customer.order(:name)
+    render :index
   end
 
   def missing_email
     @pageName = "Customers missing email address"
     @customers = Customer.where(email: '')
+    render :index
   end
 
 end
